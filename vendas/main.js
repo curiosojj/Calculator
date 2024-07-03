5c3b65534d8b7e4adc4709a1cf1e18d35062f521
James Steves <115361697+curiosojj@users.noreply.github.com>
2024-07-03 16:40:41 -0300

    Add files via upload
    Today i work in the project

--- vendas/main.js
+++ vendas/main.js
@@ -1,32 +1,21 @@
 const Button = document.querySelector('#bt');
 const Tab = document.querySelector('#tab');
 const Descont = document.querySelector('#descont');
-const Ipi = document.querySelector('#ipi');
-const Fat = document.querySelector('#fat');
 const DescResult = document.querySelector('.desconto');
-const IpiResult = document.querySelector('.ipi')
-const FatResult = document.querySelector('.fat')
 const FinalResult = document.querySelector('.final');
-const Percentual = 100;
+
 
 
 
 
 Button.addEventListener('click', ()=>{
-    let tabResult = Tab.value / Percentual * Descont.value;
-    let ipiValor = tabResult / Percentual * Ipi.value ;
-    let fatValor = tabResult / Percentual * Fat.value ;
-    let valorFinal = Tab.value - tabResult + fatValor + ipiValor ;
+let calculo = Tab.value * Descont.value / 100;
+let valorFinal = Tab.value - calculo ;
     
     
     
 
-    
-    
-
-    DescResult.innerHTML = `.desconto: ${tabResult}`;
-    IpiResult.innerHTML =  `.ipi: ${ipiValor}`;
-    FatResult.innerHTML =  `.fat: ${fatValor}`;
+    DescResult.innerHTML = `.desconto: ${calculo}`;
     FinalResult.innerHTML = `.final: ${valorFinal}`;
     
 });
